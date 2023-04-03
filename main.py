1d62dfc9615f01f674fa82bda6a542c805a7102e
ZhigarevOleg <111458739+ZhigarevOleg@users.noreply.github.com>
2023-04-03 15:08:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = int(input("Введите желаемую сумму:"))
 deposit=list(per_cent.values())
@@ -10,9 +6,3 @@ for i in range(len(deposit)):
 print(f"{deposit}- Накопленные средства за год вклада")
 deposit_max=max(deposit)
 print("Максимальная сумма, которую вы можете заработать —",deposit_max)
-
-
-
-
-
-
